1c4fc44a155f8f8172e8cf820ab06959f501089f
goodieboy <oyedeleyusuff@gmail.com>
2022-08-29 10:04:18 +0100

    it is time

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-*(2$nwbdk(trtifv&aa4(uzdpu26j_a)5381sa6d8%s_0)!5e8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []#'myweatherapptest.herokuapp.com'
+ALLOWED_HOSTS = ["myweatherapptest.herokuapp.com"]#'myweatherapptest.herokuapp.com'
 
 
 # Application definition
